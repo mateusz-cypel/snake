7fa4a35b1a4248640d885ef16ab2d3bf530adb29
Mateusz Cypel <mateusz.cypel.dev@gmail.com>
2024-07-02 20:40:16 +0200

    add snake energy handler

---
+++ handlers/snake_energy_handler.py
@@ -0,0 +1,26 @@
+from models import Snake, Fruit
+
+
+class SnakeEnergyHandler:
+    def __init__(self, snake: Snake):
+        self.snake = snake
+        self._energy = snake.energy * 10
+        self._max_energy = 100 * 10
+
+    def move(self) -> None:
+        self._energy -= 1
+        self._energy = min(self._energy, self._max_energy)
+        self.snake.energy = self._energy // 10
+
+    def hit_border(self) -> None:
+        self._energy -= 20 * 10
+        self._energy = min(self._energy, self._max_energy)
+        self.snake.energy = self._energy // 10
+
+    def eat_fruit(self, fruit: Fruit) -> None:
+        self._energy += fruit.energy * 10
+        self._energy = min(self._energy, self._max_energy)
+        self.snake.energy = self._energy // 10
+
+    def is_alive(self) -> bool:
+        return self.snake.energy > 0
